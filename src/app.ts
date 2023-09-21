825f0896c57a3617ad695600966dca85cbb425e8
Stephan Eberle <stephan.eberle@onlyfy.com>
2023-09-21 09:06:02 +0200

    update code docs

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,20 @@ import avro from 'avsc';
 
 // ====[ CONFIG ]======================
 const SCHEMA_ID = 2
-const SCHEMA_REGISTRY_URL= "http://localhost:8081/schemas/ids"
+const SCHEMA_REGISTRY_URL = "http://localhost:8081/schemas/ids"
 // ====================================
 
 
-// Here we have some schema
+/**
+* Defines an Avro schema for a message record.
+* 
+* This creates an Avro schema definition for a record type 
+* with the name 'HelloWorldMessage' and two string fields -
+* 'message' and 'sender'.
+* 
+* The schema can be used to validate JSON data against the
+* defined schema.
+*/
 const defaultSchema = avro.Type.forSchema({
     type: 'record',
     name: 'HelloWorldMessage',      // the schema ID
@@ -16,39 +25,52 @@ const defaultSchema = avro.Type.forSchema({
     ]
 });
 
-async function getSchema(id: number):Promise<string> {
+/**
+ * Retrieves the Avro schema for the given schema ID from the schema registry.
+ * 
+ * @param id - The schema ID to retrieve
+ * @returns A Promise resolving to the schema string
+ * 
+ * This makes a GET request to the schema registry URL using the provided schema ID.
+ * It returns a Promise that resolves with the schema string if the request succeeded, 
+ * or rejects with an error if not.
+ * 
+ * The response is typed and validated before returning the schema.
+ * Any errors are caught and logged before rejecting the Promise.
+ */
+async function getSchema(id: number): Promise<string> {
 
-    type GetSchemaResponse  = {
+    type GetSchemaResponse = {
         schema: string;
     };
 
     try {
-      // 👇️ const response: Response
-      const response = await fetch(`${SCHEMA_REGISTRY_URL}/${id}`, {
-        method: 'GET',
-        headers: {
-          Accept: 'application/json',
-        },
-      });
-  
-      if (!response.ok) {
-        throw new Error(`Error! status: ${response.status}`);
-      }
-  
-      const result = (await response.json()) as GetSchemaResponse;
+        // 👇️ const response: Response
+        const response = await fetch(`${SCHEMA_REGISTRY_URL}/${id}`, {
+            method: 'GET',
+            headers: {
+                Accept: 'application/json',
+            },
+        });
 
-      return result.schema as string;
+        if (!response.ok) {
+            throw new Error(`Error! status: ${response.status}`);
+        }
+
+        const result = (await response.json()) as GetSchemaResponse;
+
+        return result.schema as string;
 
     } catch (error) {
-      if (error instanceof Error) {
-        console.log('error message: ', error.message);
-        return error.message;
-      } else {
-        console.log('unexpected error: ', error);
-        return 'An unexpected error occurred';
-      }
+        if (error instanceof Error) {
+            console.log('error message: ', error.message);
+            return error.message;
+        } else {
+            console.log('unexpected error: ', error);
+            return 'An unexpected error occurred';
+        }
     }
-  }
+}
 
 // Very simple validation of some JSON thingy using the hard-coded schema
 export function Validate(input: string): boolean {
